11b0042dc46d5540da7a65513df48da2177f54c6
Archit Narwaria <128370997+Archit9121@users.noreply.github.com>
2026-06-05 01:02:22 +0530

    Data Science interactive guide

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/data-science-cheat-sheet-python/', // Must match your exact GitHub repository name
+  base: '/data-science-cheat-sheet-python/',
 })
